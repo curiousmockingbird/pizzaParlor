2ad5dc38f2923ef2cc305cf1ff3fee9da1c2b31f
Harold_Mesa <harold.mesa@icloud.com>
2022-08-26 15:52:12 -0500

    Add UI to index.html. WIP: Working on UI logic for accesing checkbox values in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,20 @@ Pizza.prototype.setTotalPrice = function() {
   return this.setToppingPrice() + this.setSizePrice();
 };
 
+// UI Logic
 
+function handleFormSubmission(event) {
+  event.preventDefault();
+  console.log("Test");
 
+  let pizza = new Pizza();
+  let toppingSelection = document.querySelector("input[name='select-topping']:checked").value;
+
+  console.log(toppingSelection);
+}
+
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", handleFormSubmission);
+})
 
