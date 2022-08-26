98912778500bc0ef838781ffd87f961571f841a2
Harold_Mesa <harold.mesa@icloud.com>
2022-08-26 13:03:00 -0500

    Pass 4th TDD for the Pizza() constructor

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,11 @@ Pizza.prototype.setPizzaProperties = function(toppings,size) {
   this.size = [size];
 };
 
-
+Pizza.prototype.setPrice = function() {
+  if (this.toppings.includes(1)) {
+    return "10.00";
+  }
+};
 
 
 
